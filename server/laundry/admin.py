e706da7a41db92ae465bee13d8802b773bd57c71
Leon Lee <93leonlee@gmail.com>
2014-03-05 00:36:48 -0500

    add the models into admin

--- server/laundry/admin.py
+++ server/laundry/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from laundry.models import Building, LaundryRoom, Machine
+
+admin.site.register(Building)
+admin.site.register(LaundryRoom)
+admin.site.register(Machine)
 
 # Register your models here.
