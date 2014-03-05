398dc1057f1d712779516960314775fabe244d02
Leon Lee <93leonlee@gmail.com>
2014-03-05 01:46:24 -0500

    add getSides to scrape the sides(A,B) of eaach building

---
+++ server/laundry/management/commands/getSides.py
@@ -0,0 +1,30 @@
+from django.core.management.base import BaseCommand, CommandError
+import requests
+import re
+from lxml import html
+from laundry.models import Community, Building
+from django.conf import settings
+
+ROOMSTATUS_BASE_URL = getattr(settings, "ROOMSTATUS_BASE_URL", None)
+
+#custom command that iterates through all of the communities and generates their buildings class Command(BaseCommand):
+class Command(BaseCommand):
+	def handle(self, *args, **options):
+		communities = Community.objects.all()
+
+		for community in communities:
+			#load the page with the locationId
+			page = requests.get(ROOMSTATUS_BASE_URL + community.locationId)
+
+			#parse the html so we can use xpath
+			tree = html.fromstring(page.text)
+			laundry_links = tree.xpath('//span[@class="dormlinks"]/a')
+
+			#regex for matching the location id of each building
+			#example: 'showRoomStatus.i?locationId=1008750'
+			regex = re.compile('\d+')
+
+			for item in laundry_links:
+				locationId = regex.search(item.attrib['href']).group()
+				building, created = Building.objects.get_or_create(name=item.text, locationId=locationId)
+				community.buildings.add(building)
