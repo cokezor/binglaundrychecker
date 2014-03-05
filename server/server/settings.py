599ba932d849233efb3a4018f086fb99802f34e4
Leon Lee <93leonlee@gmail.com>
2014-03-05 01:44:54 -0500

    add machine status base url

--- server/server/settings.py
+++ server/server/settings.py
@@ -112,3 +112,4 @@ REST_FRAMEWORK = {
 
 ROOMSTATUS_BASE_URL = 'http://binghamton-asi.esuds.net/RoomStatus/showRoomStatus.i?locationId='
 ESUDS_BING_URL = 'http://binghamton-asi.esuds.net/RoomStatus/showRoomStatus.i?locationId=6788'
+MACHINESTATUS_BASE_URL = 'http://binghamton-asi.esuds.net/RoomStatus/machineStatus.i?bottomLocationId='
