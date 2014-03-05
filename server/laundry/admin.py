03c4d5fc36498195bebba0e683ad3db3c89b9047
Leon Lee <93leonlee@gmail.com>
2014-03-05 01:48:58 -0500

    add side to admin

--- server/laundry/admin.py
+++ server/laundry/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from laundry.models import Building, Community, Machine
+from laundry.models import Building, Community, Machine, Side
 
 admin.site.register(Building)
 admin.site.register(Community)
 admin.site.register(Machine)
+admin.site.register(Side)
 
 # Register your models here.
