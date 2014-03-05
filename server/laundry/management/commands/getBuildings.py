3323f605f2528357c697d7aaf6f44e98f9256b59
Leon Lee <93leonlee@gmail.com>
2014-03-05 01:44:38 -0500

    getBuildings should work now; add building to buildings of each community

--- server/laundry/management/commands/getBuildings.py
+++ server/laundry/management/commands/getBuildings.py
@@ -3,22 +3,28 @@ import requests
 import re
 from lxml import html
 from laundry.models import Community, Building
+from django.conf import settings
 
-#custom command that iterates through all of the communities and generates their buildings
+ROOMSTATUS_BASE_URL = getattr(settings, "ROOMSTATUS_BASE_URL", None)
+
+#custom command that iterates through all of the communities and generates their buildings class Command(BaseCommand):
 class Command(BaseCommand):
 	def handle(self, *args, **options):
+		communities = Community.objects.all()
 
-		#load the page
-		page = requests.get('http://binghamton-asi.esuds.net/RoomStatus/showRoomStatus.i?locationId=6788')
+		for community in communities:
+			#load the page with the locationId
+			page = requests.get(ROOMSTATUS_BASE_URL + community.locationId)
 
-		#parse the html so we can use xpath
-		tree = html.fromstring(page.text)
-		building_list = tree.xpath('//li/a')
+			#parse the html so we can use xpath
+			tree = html.fromstring(page.text)
+			laundry_links = tree.xpath('//span[@class="dormlinks"]/a')
 
-		#regex for matching the location id of each building
-		#example: 'showRoomStatus.i?locationId=1008750'
-		regex = re.compile('\d+')
+			#regex for matching the location id of each building
+			#example: 'showRoomStatus.i?locationId=1008750'
+			regex = re.compile('\d+')
 
-		for item in building_list:
-			locationId = regex.search(item.attrib['href']).group()
-			building, created = Building.objects.get_or_create(name=item.text, locationId=locationId)
+			for item in laundry_links:
+				locationId = regex.search(item.attrib['href']).group()
+				building, created = Building.objects.get_or_create(name=item.text, locationId=locationId)
+				community.buildings.add(building)
