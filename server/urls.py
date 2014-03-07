0b7d6f480499e35ddf6f331ee2796d9dbecd2cab
Leon Lee <93leonlee@gmail.com>
2014-03-06 21:40:17 -0500

    remove api links in url

--- server/urls.py
+++ server/urls.py
@@ -14,6 +14,4 @@ urlpatterns = patterns('',
 
     url(r'^', include('laundry.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include(router.urls)), 
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 )
