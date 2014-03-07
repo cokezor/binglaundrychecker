c65ea49bd63fa258fef93aa92d7bd3159e975a3f
Leon Lee <93leonlee@gmail.com>
2014-03-06 21:10:56 -0500

    also part of the move

--- server/laundry/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from laundry.models import Building, Community, Side
-
-admin.site.register(Building)
-admin.site.register(Community)
-admin.site.register(Side)
-
-# Register your models here.
--- server/laundry/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, url
-
-urlpatterns = patterns('laundry.views',
-	url(r'^get_buildings/(?P<name>\w+)/$', 'get_buildings'),
-	url(r'^communities', 'get_communities'),
-	url(r'^status/(?P<name>\w+)/?', 'get_laundry_status'),
-)
